f19c4da7065c216afaddf35adb5dc63abc64229c
Jason Bartley <83716676+jasonbartley3990@users.noreply.github.com>
2024-10-16 19:09:49 -0400

    Initial Commit

---
+++ KissNails/KissNailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KissNailsApp.swift
+//  KissNails
+//
+//  Created by Jason bartley on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct KissNailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
